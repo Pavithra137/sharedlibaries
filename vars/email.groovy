7e83ec7919beb727c6e114917f638349520655a7
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-11-25 19:18:36 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,4 +1,14 @@
 def call(BUILD_URL,JOB_NAME,BUILD_NUMBER,BUILD_STATUS)
 {
-  echo "Build URL: $BUILD_URL \n Project: $JOB_NAME \n Build Number:     $BUILD_NUMBER \n Build Result: $BUILD_STATUS"
+  post {
+        always {
+            emailext body: '''Environment:       Production
+Build URL:          $BUILD_URL
+Project:              $JOB_NAME
+Build Number:     $BUILD_NUMBER
+Build Result:       $BUILD_STATUS
+''', subject: 'Notification', to: 'coolpavi193@gmail.com'
+        }
+    }
+
 }
