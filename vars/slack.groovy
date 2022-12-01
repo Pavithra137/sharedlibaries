9b94413ba76bfd1654d6886192b8a8b688cbf8ef
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-12-01 13:05:24 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -10,12 +10,22 @@ Build Result:       $BUILD_STATUS
 ''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
   }
 //   else if( buildResult == "FAILURE" ) { 
-//     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+//     emailext body: '''Environment:       Production
+// Build URL:          $BUILD_URL
+// Project:              $JOB_NAME
+// Build Number:     $BUILD_NUMBER
+// Build Result:       $BUILD_STATUS
+// ''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
 //   }
 //   else if( buildResult == "UNSTABLE" ) { 
 //     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
 //   }
-//   else {
-//     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-//   }
+  else {
+    	emailext body: '''Environment:       Production
+Build URL:          $BUILD_URL
+Project:              $JOB_NAME
+Build Number:     $BUILD_NUMBER
+Build Result:       $BUILD_STATUS
+''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
+  }
 }
