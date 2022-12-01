bc10aef9be2fbb4d7093d357e875399f63121c27
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-12-01 13:01:02 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -2,15 +2,20 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    emailext body: '''Environment:       Production
+Build URL:          $BUILD_URL
+Project:              $JOB_NAME
+Build Number:     $BUILD_NUMBER
+Build Result:       $BUILD_STATUS
+''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
   }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
+//   else if( buildResult == "FAILURE" ) { 
+//     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+//   }
+//   else if( buildResult == "UNSTABLE" ) { 
+//     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+//   }
+//   else {
+//     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+//   }
 }
