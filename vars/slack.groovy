f5e0b4f6e24cd66c65ab72ecff0691c7ffedb696
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-12-01 15:18:58 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,31 +1,32 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
+def call() {
+//   if ( buildResult == "SUCCESS" )
+  //{
     emailext body: '''Environment:       Production
 Build URL:          $BUILD_URL
 Project:              $JOB_NAME
 Build Number:     $BUILD_NUMBER
 Build Result:       $BUILD_STATUS
 ''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
-  }
-  else if( buildResult == "FAILURE" ) { 
-    emailext body: '''Environment:       Production
-Build URL:          $BUILD_URL
-Project:              $JOB_NAME
-Build Number:     $BUILD_NUMBER
-Build Result:       $BUILD_STATUS
-''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
-  }
-//   else if( buildResult == "UNSTABLE" ) { 
-//     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
 //   }
-  else {
-    	emailext body: '''Environment:       Production
-Build URL:          $BUILD_URL
-Project:              $JOB_NAME
-Build Number:     $BUILD_NUMBER
-Build Result:       $BUILD_STATUS
-''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
-  }
+//   else if( buildResult == "FAILURE" ) { 
+//     emailext body: '''Environment:       Production
+// Build URL:          $BUILD_URL
+// Project:              $JOB_NAME
+// Build Number:     $BUILD_NUMBER
+// Build Result:       $BUILD_STATUS
+// ''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
+//   }
+// //   else if( buildResult == "UNSTABLE" ) { 
+// //     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+// //   }
+//   else {
+//     	emailext body: '''Environment:       Production
+// Build URL:          $BUILD_URL
+// Project:              $JOB_NAME
+// Build Number:     $BUILD_NUMBER
+// Build Result:       $BUILD_STATUS
+// ''', subject: 'STC IMS JENKINS BUILD STATUS', to: 'coolpavi193@gmail.com'
+//   }
 }
