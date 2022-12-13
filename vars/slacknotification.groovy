5a8d82dcc925cc70f0c51ecfa5a558d90e75cdd1
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-12-13 15:27:34 +0530

    Update slacknotification.groovy

--- vars/slacknotification.groovy
+++ vars/slacknotification.groovy
@@ -2,7 +2,7 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" || buildResult == "FAILURE" || buildResult == "UNSTABLE" ) {
-    slackSend channel: '#devops', message: "jenkins status Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${env.BUILD_STATUS}"
+    slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${env.BUILD_RESULT}"
   }
 //   else if( buildResult == "FAILURE" ) { 
 //     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
