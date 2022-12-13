1cd5cb955f42b48b7cdee4b8d14c68ff4cf9199c
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-12-13 16:00:30 +0530

    Update slacknotification.groovy

--- vars/slacknotification.groovy
+++ vars/slacknotification.groovy
@@ -4,12 +4,12 @@ def call(String buildResult) {
   if ( buildResult == "SUCCESS") {
     slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${currentBuild.currentResult}"
   }
-  else if ( buildResult == "FAILURE"  ) {
-    slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${currentBuild.currentResult}"
-  }
-  else if ( buildResult == "UNSTABLE"  ) {
-    slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${currentBuild.currentResult}"
-  }
+//   else if ( buildResult == "FAILURE"  ) {
+//     slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${currentBuild.currentResult}"
+//   }
+//   else if ( buildResult == "UNSTABLE"  ) {
+//     slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${currentBuild.currentResult}"
+//   }
   else {
     slackSend channel: '#devops', message: "jenkins status \n Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${currentBuild.currentResult}"
   }
