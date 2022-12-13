b8ff55a0e8ab08b9cc8b2339dc470e003388c6e2
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2022-12-13 15:26:28 +0530

    Update slacknotification.groovy

--- vars/slacknotification.groovy
+++ vars/slacknotification.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
 def call(String buildResult) {
-  if ( buildResult == "PAVITRA" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  if ( buildResult == "SUCCESS" || buildResult == "FAILURE" || buildResult == "UNSTABLE" ) {
+    slackSend channel: '#devops', message: "jenkins status Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${env.BUILD_STATUS}"
   }
 //   else if( buildResult == "FAILURE" ) { 
 //     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
 //   }
 //   else if( buildResult == "UNSTABLE" ) { 
 //     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+// //   }
+//   else {
+//     slackSend channel: '#devops', message: "jenkins status Build_URL : ${env.BUILD_URL} \n Job_Name : ${env.JOB_NAME}\n  Build_Number: ${env.BUILD_NUMBER} \n BuilD_status: ${env.BUILD_STATUS}"
 //   }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
 }
