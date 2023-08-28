f70a5e8f5aa86f28c81fa4eea44ace7681694862
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2023-08-28 14:48:49 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,5 +1,14 @@
 #!/usr/bin/env groovy
 
-def newslack() {
-slackSend channel: '#devops', message: "jenkins status Build_URL : ${BUILD_URL} \n Job_Name : ${JOB_NAME}\n  Build_Number: ${BUILD_NUMBER} \n BuilD_status: ${BUILD_STATUS}"
+// def newslack() {
+// slackSend channel: '#devops', message: "jenkins status Build_URL : ${BUILD_URL} \n Job_Name : ${JOB_NAME}\n  Build_Number: ${BUILD_NUMBER} \n BuilD_status: ${BUILD_STATUS}"
+// }
+
+def call() {
+ sh """
+ ls
+ mkdir new
+ echo "hello" > 1.txt
+ cat 1.txt
+ """
 }
